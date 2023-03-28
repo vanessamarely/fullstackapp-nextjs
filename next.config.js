3f57ea33f292bb88f7a326c04dc82ebe9839e802
vanessamarely <vanessamarely@gmail.com>
2023-03-28 18:26:38 -0500

    omit linter errors

--- next.config.js
+++ next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    // !! WARN !!
+    //Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 }
 
